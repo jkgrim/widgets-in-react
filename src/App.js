1d2f737c11a638c46436590df7d6dd0a74446e34
jkgrim <j.grimshaw37@gmail.com>
2022-08-03 21:13:07 -0600

    created dice roll widget

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import DiceRoll from "./components/DiceRoll";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <DiceRoll />
+
+      {/* 
+      add 2 numbers 
+      greeter (first name, last name, greet button with alert "hello -fname -lname")
+      search filter
+      */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/DiceRoll.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function DiceRoll() {
+  const [num, setNum] = useState(1);
+  const [num2, setNum2] = useState(1);
+
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  function rollDice(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  async function handleClick() {
+    for (let i = 0; i < 20; i++) {
+      setNum(rollDice(1, 6));
+      setNum2(rollDice(1, 6));
+      await sleep(100);
+    }
+  }
+
+  return (
+    <div className="dice-container">
+      <div className="dice-wrapper">
+        <h2 className="dice-1">{num}</h2>
+        <h2 className="dice-2">{num2}</h2>
+      </div>
+      <button onClick={handleClick}>Roll the Dice</button>
+      <hr />
+    </div>
+  );
+}
