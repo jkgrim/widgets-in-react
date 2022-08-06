c466e73e6450966e66b87ed6756e26301dfb7566
jkgrim <j.grimshaw37@gmail.com>
2022-08-06 16:53:40 -0600

    switched to scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
-import "./App.css";
+import "./App.scss";
 import NavBar from "./navigation/NavBar";
 import Home from "./pages/Home";
 import DiceRoll from "./pages/DiceRoll";
