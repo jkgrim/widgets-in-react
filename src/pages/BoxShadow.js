a7a0c4492d310775f266a461c0db8305acd19293
jkgrim <j.grimshaw37@gmail.com>
2022-08-08 14:44:23 -0600

    finishes styling pizza builder, adds box shadow functionality and styling

--- src/pages/BoxShadow.js
+++ src/pages/BoxShadow.js
@@ -1,3 +1,77 @@
+import { useState } from "react";
+
 export default function BoxShadow() {
-  return <h1>Box Shadow Generator</h1>;
+  const [xAxis, setXAxis] = useState(5);
+  const [yAxis, setYAxis] = useState(5);
+  const [blur, setBlur] = useState(20);
+  const [opacity, setOpacity] = useState(5);
+
+  return (
+    <div className="box-shadow-container">
+      <div
+        className="preview-box"
+        style={{ boxShadow: `${xAxis}px ${yAxis}px ${blur}px ${opacity}px` }}
+      >
+        Preview
+      </div>
+
+      <div className="sliders-wrapper">
+        <div className="range-wrapper">
+          <label>X-Axis Shift: </label>
+          <input
+            id="x-axis"
+            type="range"
+            min="-50"
+            max="50"
+            steps="1"
+            value={xAxis}
+            onChange={(e) => setXAxis(e.target.value)}
+          />
+        </div>
+
+        <div className="range-wrapper">
+          <label>Y-Axis Shift: </label>
+          <input
+            id="y-axis"
+            type="range"
+            min="-50"
+            max="50"
+            steps="1"
+            value={yAxis}
+            onChange={(e) => setYAxis(e.target.value)}
+          />
+        </div>
+
+        <div className="range-wrapper">
+          <label>Blur Shift: </label>
+          <input
+            id="blur"
+            type="range"
+            min="0"
+            max="100"
+            steps="1"
+            value={blur}
+            onChange={(e) => setBlur(e.target.value)}
+          />
+        </div>
+
+        <div className="range-wrapper">
+          <label>Opacity Shift: </label>
+          <input
+            id="opacity"
+            type="range"
+            min="0"
+            max="100"
+            steps="1"
+            value={opacity}
+            onChange={(e) => setOpacity(e.target.value)}
+          />
+        </div>
+
+        <div className="output-code">
+          box-shadow: ({xAxis}px {yAxis}px {blur}px {opacity}px)
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/PizzaBuilder.js
+++ src/pages/PizzaBuilder.js
@@ -9,39 +9,41 @@ export default function PizzaBuilder() {
 
   return (
     <div className="pizza-builder-container">
-      <h1>Lets Order a Pizza!</h1>
+      <div className="pizza-builder-wrapper">
+        <h1>Lets Order a Pizza!</h1>
 
-      <div className="size-wrapper">
-        <h3>What size would you like?</h3>
-        <form className="radio-btn-wrapper">
-          <input
-            type="radio"
-            name="service-btn"
-            value="small"
-            checked={selected === "small"}
-            onChange={handleChange}
-          />{" "}
-          Small
-          <input
-            type="radio"
-            name="service-btn"
-            value="medium"
-            checked={selected === "medium"}
-            onChange={handleChange}
-          />{" "}
-          Medium
-          <input
-            type="radio"
-            name="service-btn"
-            value="large"
-            checked={selected === "large"}
-            onChange={handleChange}
-          />{" "}
-          Large
-        </form>
+        <div className="size-wrapper">
+          <h3>What size would you like?</h3>
+          <form className="radio-btn-wrapper">
+            <input
+              type="radio"
+              name="service-btn"
+              value="small"
+              checked={selected === "small"}
+              onChange={handleChange}
+            />{" "}
+            Small
+            <input
+              type="radio"
+              name="service-btn"
+              value="medium"
+              checked={selected === "medium"}
+              onChange={handleChange}
+            />{" "}
+            Medium
+            <input
+              type="radio"
+              name="service-btn"
+              value="large"
+              checked={selected === "large"}
+              onChange={handleChange}
+            />{" "}
+            Large
+          </form>
 
-        <div className="selected-pizza">
-          One {selected} pizza, coming right up!
+          <div className="selected-pizza">
+            One {selected} pizza, coming right up!
+          </div>
         </div>
       </div>
     </div>
